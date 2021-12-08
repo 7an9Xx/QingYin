5f0a2ae2227ee84de17d947f1d2257dabb2408bf
7an9Xx <1691166186@qq.com>
2021-12-08 11:33:47 +0800

    第一次提交

---
+++ src/main/java/qy/qingyinweb/util/JdbcUtils.java
@@ -0,0 +1,16 @@
+package qy.qingyinweb.util;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
+import javax.sql.DataSource;
+
+
+public class JdbcUtils {
+    public static DataSource source=new ComboPooledDataSource("intergalactoApp");
+
+    public static DataSource getDataSource(){
+        return source;
+    }
+
+
+}
